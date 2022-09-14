160eb06c7eab7225f764778fcc3aeb4d3ede92f8
asdasd <92120487+Ich-mag-dich@users.noreply.github.com>
2022-09-15 02:06:32 +0900

    setting

---
+++ src/components/tabTest/Home2.vue
@@ -0,0 +1,3 @@
+<template>
+	<div class="tab">This is Home2 component</div>
+</template>
---
+++ src/components/test1.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+    import {ref} from 'vue'
+    defineProps<{ testMessage: string }>()
+    const testCount = ref(0)
+</script>
+
+<template>
+    <div class="testDiv">
+
+        <h1> /// </h1>
+        <h1> {{ testMessage }}</h1>
+        <div class="testClass">
+            <button type="button" @click="testCount++">
+                count is {{ testCount }}
+            </button>
+        </div>
+        <h1> /// </h1>
+    </div>
+</template>
+
+
+<style scoped>
+    .testClass {
+        color: red;
+    }
+    .testDiv {
+        border-style: solid;
+        border-width: 2px;
+    }
+</style>
